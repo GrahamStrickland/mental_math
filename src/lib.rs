c1dff09f505b42a85107f1bb59cc0290b5627e9e
Graham Strickland <grahamhstrickland@gmail.com>
2025-09-12 17:47:25 +0200

    Add cross multiplication exercise and colorised help output

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use chrono::{Duration, NaiveDate};
 use rand::Rng;
+use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 
 pub fn random_date_in_range(
     rng: &mut rand::rngs::ThreadRng,
@@ -11,20 +12,38 @@ pub fn random_date_in_range(
     start + Duration::days(random_days)
 }
 
-pub fn help() {
-    println!("\
+pub fn calendar_dates_help() {
+    let mut stdout = StandardStream::stdout(ColorChoice::Always);
+
+    println!(
+        "\
 ===========================
 Calculating Calendar Dates:
 ===========================
+"
+    );
+
+    print_color(
+        "\
 Century:
     1600s / 2000s / 2400s /…  +2
     1700s / 2100s / 2500s /…  +0
     1800s / 2200s / 2600s /…  +5
     1900s / 2300s / 2700s /…  +3
 
+",
+        Color::Red,
+        &mut stdout,
+    );
+
+    print_color("\
 Year:
     Divide the year by 4 and ignore any remainder. Then add this to the original year. Find the remainder when dividing by 7.
 
+", Color::Yellow, &mut stdout);
+
+    print_color(
+        "\
 Month:
     January:   +4
     February:  +0
@@ -39,10 +58,87 @@ Month:
     November:  +0
     December:  +2
 
+",
+        Color::Green,
+        &mut stdout,
+    );
+
+    print_color("\
 Day:
     Just use the date itself. But to simplify calculation later, it is better to find the remainder when dividing by 7.
 
+", Color::Blue, &mut stdout);
+
+    print_color("\
 Add together the 4 contributions, but if the date is in January or February in a leap year, you must subtract one.
-");
+
+", Color::White, &mut stdout);
 }
 
+pub fn cross_multiplication_help() {
+    let mut stdout = StandardStream::stdout(ColorChoice::Always);
+
+    println!(
+        "\
+=====================
+Cross Multiplication:
+=====================
+Basic Method for Small Numbers:
+When calculating a multiplication where one of the numbers is small, such as 68435 × 18, 
+it may be fastest to simply add together multiples of the smaller number:
+"
+    );
+
+    print_color("         5", Color::Green, &mut stdout);
+    print_color(" × 18 =  ", Color::White, &mut stdout);
+    print_color("9", Color::Red, &mut stdout);
+    print_color("0", Color::Blue, &mut stdout);
+    print_color(" ⇒ ......", Color::White, &mut stdout);
+    print_color("0\n", Color::Blue, &mut stdout);
+
+    print_color("     9", Color::Red, &mut stdout);
+    print_color(" + ", Color::White, &mut stdout);
+    print_color("3", Color::Green, &mut stdout);
+    print_color(" × 18 =  ", Color::White, &mut stdout);
+    print_color("6", Color::Red, &mut stdout);
+    print_color("3", Color::Blue, &mut stdout);
+    print_color(" ⇒ .....", Color::White, &mut stdout);
+    print_color("3", Color::Blue, &mut stdout);
+    print_color("0\n", Color::White, &mut stdout);
+
+    print_color("     6", Color::Red, &mut stdout);
+    print_color(" + ", Color::White, &mut stdout);
+    print_color("4", Color::Green, &mut stdout);
+    print_color(" × 18 =  ", Color::White, &mut stdout);
+    print_color("7", Color::Red, &mut stdout);
+    print_color("8", Color::Blue, &mut stdout);
+    print_color(" ⇒ ....", Color::White, &mut stdout);
+    print_color("8", Color::Blue, &mut stdout);
+    print_color("30\n", Color::White, &mut stdout);
+
+    print_color("     7", Color::Red, &mut stdout);
+    print_color(" + ", Color::White, &mut stdout);
+    print_color("8", Color::Green, &mut stdout);
+    print_color(" × 18 = ", Color::White, &mut stdout);
+    print_color("15", Color::Red, &mut stdout);
+    print_color("1", Color::Blue, &mut stdout);
+    print_color(" ⇒ ...", Color::White, &mut stdout);
+    print_color("1", Color::Blue, &mut stdout);
+    print_color("830\n", Color::White, &mut stdout);
+
+    print_color("    15", Color::Red, &mut stdout);
+    print_color(" + ", Color::White, &mut stdout);
+    print_color("6", Color::Green, &mut stdout);
+    print_color(" × 18 = ", Color::White, &mut stdout);
+    print_color("123", Color::Blue, &mut stdout);
+    print_color(" ⇒ ", Color::White, &mut stdout);
+    print_color("123", Color::Blue, &mut stdout);
+    print_color("1830\n\n", Color::White, &mut stdout);
+}
+
+fn print_color(string: &str, color: Color, stdout: &mut StandardStream) {
+    match stdout.set_color(ColorSpec::new().set_fg(Some(color))) {
+        Ok(_) => print!("{}", string),
+        Err(e) => eprintln!("{}", e),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,63 @@
-use std::env;
+use rand::Rng;
+use std::convert::{TryFrom, TryInto};
 use std::io;
 
 use chrono::{Datelike, NaiveDate, Weekday};
-use mental_math::{help, random_date_in_range};
+use mental_math::{calendar_dates_help, cross_multiplication_help, random_date_in_range};
+
+static MAX_BIG_NUMBER: i32 = 99999;
+static MAX_SMALL_NUMBER: i32 = 99;
+
+enum Choices {
+    CalendarDates = 1,
+    Multiplication,
+}
+
+impl TryFrom<u32> for Choices {
+    type Error = ();
+
+    fn try_from(i: u32) -> Result<Self, Self::Error> {
+        match i {
+            x if x == Choices::CalendarDates as u32 => Ok(Choices::CalendarDates),
+            x if x == Choices::Multiplication as u32 => Ok(Choices::Multiplication),
+            _ => Err(()),
+        }
+    }
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    println!(
+        "\
+===========
+MENTAL MATH
+===========
+Choose an exercise:
+1 = Calculate Calendar Dates 
+2 = Cross Multiplication"
+    );
 
-    if args.len() > 1 && &args[1] == "help" {
-        help();
-        return
-    }
+    let mut choice = String::new();
 
-    println!("Enter the day of the week:");
+    io::stdin()
+        .read_line(&mut choice)
+        .expect("Failed to read line");
+
+    let choice: u32 = choice.trim().parse().expect("Please type a number!");
 
     let mut rng = rand::rng();
+
+    match choice.try_into() {
+        Ok(Choices::CalendarDates) => calculate_calendar_dates(&mut rng),
+        Ok(Choices::Multiplication) => cross_multiplication(&mut rng),
+        Err(_) => eprintln!("Unknown option, please select either 1 or 2"),
+    }
+}
+
+fn calculate_calendar_dates(rng: &mut rand::rngs::ThreadRng) {
+    println!("Enter the day of the week:");
+
     let rand_date = random_date_in_range(
-        &mut rng,
+        rng,
         NaiveDate::from_ymd_opt(1600, 1, 1).unwrap(),
         NaiveDate::from_ymd_opt(2099, 12, 31).unwrap(),
     );
@@ -32,12 +73,52 @@ fn main() {
 
         let weekday = match answer.trim().parse::<Weekday>() {
             Ok(weekday) => weekday,
-            Err(_) => continue,
+            Err(_) => {
+                if answer.trim() == "help" || answer.trim() == "--help" || answer.trim() == "-h" {
+                    calendar_dates_help();
+                }
+                continue;
+            }
         };
 
         println!("Your answer: {weekday}");
 
         if weekday == rand_date.weekday() {
+            println!("You are correct!");
+            break;
+        }
+        println!("Try again!");
+    }
+}
+
+fn cross_multiplication(rng: &mut rand::rngs::ThreadRng) {
+    println!("Enter the answer:");
+
+    let big_number = rng.random_range(0..MAX_BIG_NUMBER);
+    let small_number = rng.random_range(0..MAX_SMALL_NUMBER);
+
+    loop {
+        println!("{} x {}", big_number, small_number);
+
+        let mut answer = String::new();
+
+        io::stdin()
+            .read_line(&mut answer)
+            .expect("Failed to read line");
+
+        let answer: i32 = match answer.trim().parse() {
+            Ok(i) => i,
+            Err(_) => {
+                if answer.trim() == "help" || answer.trim() == "--help" || answer.trim() == "-h" {
+                    cross_multiplication_help();
+                }
+                continue;
+            }
+        };
+
+        println!("Your answer: {answer}");
+
+        if answer == big_number * small_number {
             println!("You are correct!");
             break;
         }
